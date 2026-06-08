9eb84e3fd66a8045177ea63ff802a50c1890e3f2
Matt Randell <randellma@gmail.com>
2026-06-08 00:24:06 -0400

    bump service worker

--- inventory/web/public/sw.js
+++ inventory/web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'inventory-shell-v2';
+const CACHE = 'inventory-shell-v3';
 const SHELL = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
